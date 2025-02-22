fa73b281c0229c5d2d1b12c0c77cccf9575508ea
Matthew Akrigg <138000221+matthewakrigg05@users.noreply.github.com>
2025-02-22 19:56:57 +0000

    refactor: cleaning up solution to look simpler, runs the same

--- Solutions/Leetcode7-ReverseInteger.py
+++ Solutions/Leetcode7-ReverseInteger.py
@@ -1,31 +1,18 @@
 def reverse(x):
-    reverse = []
-    res = ''
+    if x != 0:
+        res = ''
+        num = list(str(abs(x)))
 
-    if x < 0:
-        num =  list(str(abs(x)))
-       
-        for i in num:
-            reverse.insert(0, i)
+        for i in num[::-1]:
+            res += str(i)
 
-        for i in reverse:
-            res += i
-        if -2**31 <= int(res) <= 2**31 - 1:
-            return 0 - int(res)
-
-    elif x > 0:
-        num =  list(str(abs(x)))
-       
-        for i in num:
-            reverse.insert(0, i)
-
-        for i in reverse:
-            res += i
-
-        if -2**31 <= int(res) <= 2**31 - 1:
-            return int(res)
-
-    return 0
+        if -2 ** 31 <= int(res) <= 2 ** 31 - 1:
+            if x < 0:
+                return 0 - int(res)
+            else:
+                return int(res)
+                
+        return 0
 
 print(reverse(123))
 print(reverse(-123))
